0193f7aa690cc4eb8ba330ad30c801f203771548
Genesis <lisbethgm2007@gmail.com>
2026-05-12 22:00:13 -0500

    Ejercicio 3 en c++

--- cpp/Ejercicio3_Binario.cpp
+++ cpp/Ejercicio3_Binario.cpp
@@ -7,23 +7,41 @@ struct Nodo {
     int valor;
     Nodo* izquierdo;
     Nodo* derecho;
+
     Nodo(int v) : valor(v), izquierdo(nullptr), derecho(nullptr) {}
 };
 
 int calcularAltura(Nodo* raiz) {
-    // TODO: Implementa tu lógica aquí
-    return 0;
+
+    // Caso base
+    if (raiz == nullptr) {
+        return 0;
+    }
+
+    // Calculamos altura izquierda y derecha
+    int alturaIzquierda = calcularAltura(raiz->izquierdo);
+    int alturaDerecha = calcularAltura(raiz->derecho);
+
+    // Retornamos la mayor + 1
+    return max(alturaIzquierda, alturaDerecha) + 1;
 }
 
 int main() {
+
     Nodo* raiz = new Nodo(1);
+
     raiz->derecho = new Nodo(2);
     raiz->derecho->izquierdo = new Nodo(3);
-    
+
     cout << "--- Prueba Ejercicio 3 ---" << endl;
+
     cout << "Altura esperada: 3" << endl;
-    cout << "Altura calculada: " << calcularAltura(raiz) << endl;
-    cout << "Altura de arbol nulo (esperado 0): " << calcularAltura(nullptr) << endl;
+
+    cout << "Altura calculada: "
+         << calcularAltura(raiz) << endl;
+
+    cout << "Altura de arbol nulo (esperado 0): "
+         << calcularAltura(nullptr) << endl;
 
     return 0;
 }
