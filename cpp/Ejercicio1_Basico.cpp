2ec0bee913ac635733116c51cc9c749695bbd213
Genesis <lisbethgm2007@gmail.com>
2026-05-12 21:18:59 -0500

    Ejercicio 1 completado en C++

--- cpp/Ejercicio1_Basico.cpp
+++ cpp/Ejercicio1_Basico.cpp
@@ -5,24 +5,43 @@ using namespace std;
 struct NodoN {
     int valor;
     vector<NodoN*> hijos;
+
     NodoN(int v) : valor(v) {}
 };
 
 int contarNodos(NodoN* raiz) {
-    // TODO: Implementa tu lógica aquí
-    return 0;
+
+    // Caso base
+    if (raiz == NULL) {
+        return 0;
+    }
+
+    // Contamos el nodo actual
+    int total = 1;
+
+    // Recorremos todos los hijos
+    for (NodoN* hijo : raiz->hijos) {
+        total += contarNodos(hijo);
+    }
+
+    return total;
 }
 
 int main() {
+
     // Construimos el árbol
     NodoN* n5 = new NodoN(5);
     NodoN* n6 = new NodoN(6);
+
     NodoN* n2 = new NodoN(2);
     n2->hijos.push_back(n5);
     n2->hijos.push_back(n6);
+
     NodoN* n3 = new NodoN(3);
     NodoN* n4 = new NodoN(4);
+
     NodoN* raiz = new NodoN(1);
+
     raiz->hijos.push_back(n2);
     raiz->hijos.push_back(n3);
     raiz->hijos.push_back(n4);
@@ -30,6 +49,6 @@ int main() {
     cout << "--- Prueba Ejercicio 1 ---" << endl;
     cout << "Nodos esperados: 6" << endl;
     cout << "Nodos calculados: " << contarNodos(raiz) << endl;
-    
+
     return 0;
 }
