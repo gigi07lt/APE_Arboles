ec63361e9731dde993921211024e788238439986
Genesis <lisbethgm2007@gmail.com>
2026-05-12 22:24:19 -0500

    Ejercicio 5 en C++

--- cpp/Ejercicio5_Transformacion.cpp
+++ cpp/Ejercicio5_Transformacion.cpp
@@ -5,34 +5,60 @@ struct Nodo {
     int valor;
     Nodo* izquierdo;
     Nodo* derecho;
+
     Nodo(int v) : valor(v), izquierdo(nullptr), derecho(nullptr) {}
 };
 
 Nodo* invertir(Nodo* raiz) {
-    // TODO: Implementa tu lógica aquí
+
+    // Caso base
+    if (raiz == nullptr) {
+        return nullptr;
+    }
+
+    // Intercambiamos hijos
+    Nodo* temp = raiz->izquierdo;
+    raiz->izquierdo = raiz->derecho;
+    raiz->derecho = temp;
+
+    // Aplicamos recursividad
+    invertir(raiz->izquierdo);
+    invertir(raiz->derecho);
+
     return raiz;
 }
 
 int main() {
+
     Nodo* raiz = new Nodo(1);
+
     raiz->izquierdo = new Nodo(2);
     raiz->derecho = new Nodo(3);
 
     cout << "--- Prueba Ejercicio 5 ---" << endl;
+
     cout << "Antes de invertir:" << endl;
-    cout << "Hijo Izq: " << raiz->izquierdo->valor << " | Hijo Der: " << raiz->derecho->valor << endl;
-    
+
+    cout << "Hijo Izq: "
+         << raiz->izquierdo->valor
+         << " | Hijo Der: "
+         << raiz->derecho->valor << endl;
+
     invertir(raiz);
-    
+
     cout << "\nDespues de invertir (Esperado: Izq 3 | Der 2):" << endl;
-    
-    if(raiz->izquierdo) cout << "Hijo Izq: " << raiz->izquierdo->valor;
-    else cout << "Hijo Izq: null";
-    
+
+    if(raiz->izquierdo)
+        cout << "Hijo Izq: " << raiz->izquierdo->valor;
+    else
+        cout << "Hijo Izq: null";
+
     cout << " | ";
-    
-    if(raiz->derecho) cout << "Hijo Der: " << raiz->derecho->valor << endl;
-    else cout << "Hijo Der: null" << endl;
+
+    if(raiz->derecho)
+        cout << "Hijo Der: " << raiz->derecho->valor << endl;
+    else
+        cout << "Hijo Der: null" << endl;
 
     return 0;
 }
